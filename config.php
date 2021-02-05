a7bf740a43d3771e9d64411d1ee200acec32ab58
audreyantony <web2020.audrey@gmail.com>
2021-02-05 15:33:06 +0100

    thenews

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define("DB_TYPE","mysql");
 define("DB_HOST","localhost");
 define("DB_NAME","exe_oo_1_table");
-define("DB_PORT",3307);
+define("DB_PORT",3306);
 define("DB_CHARSET","utf8");
 define("DB_LOGIN","root");
 define("DB_PWD","");
--- model/thenews/Thenews.php
+++ model/thenews/Thenews.php
@@ -7,15 +7,77 @@ class Thenews
     private string $theUserLogin;
 
     // EXERCICE créez les autres attributs (noms des champs dans le table "thenews")
-
+    private int $idNews;
+    private string $titleNews;
+    private string $textNews;
+    private string $dateNews;
 
     // EXERCICE créez le constructeur
-
+    public function __construct(array $datas) {
+        if(!empty($datas)){
+            $this->hydratation($datas);
+        }
+    }
 
     // EXERCICE créez l'hydratateur
-
+    private function hydratation(array $values){
+        foreach($values AS $key => $values){
+            $setterName = "set".ucfirst($key);
+            if(method_exists($this, $setterName)){
+                $this->$setterName($values);
+            }
+        }
+    }
 
     // EXERCICE créez les getters et setters des attributs propre à cette table, n'oubliez pas de protéger les champs avec les setters !
+    public function getIdNews() {
+        return $this->idNews;
+    }
+
+    public function getTitleNews() {
+        return html_entity_decode($this->titleNews,ENT_QUOTES);
+    }
+
+    public function getTextNews() {
+        return html_entity_decode($this->textNews,ENT_QUOTES);
+    }
+
+    public function getDateNews() {
+        return $this->dateNews;
+    }
+
+    public function setIdNews(int $idNews): void {
+            $this->idNews = $idNews;
+        }
+
+    public function setTitleNews(string $titleNews): void {
+        $title = strip_tags(trim($titleNews));
+        if(empty($title)){
+            trigger_error("Le titre de l'article ne peut pas être vide, il faut le remplir.",E_USER_NOTICE);
+        }elseif (strlen($title)>150){
+            trigger_error("Votre titre ne peut pas être trop long, il ne doit pas dépasser les 150 caractères",E_USER_NOTICE);
+        }else{
+            $this->titleNews = $title;
+        }
+    }
+
+    public function setTextNews(string $textNews): void {
+        $text = strip_tags(trim($textNews),"<br>,<p>,<div>,<a>,<img>");
+        if(empty($text)){
+            print("Vous devez renseigner un texte.");
+        }else {
+            $this->textNews = $textNews;
+        }
+    }
+
+    public function setArticleDateTime(string $dateNews): void {
+        $rgx = preg_grep("/^(\d{4})-(\d{2})-([\d]{2}) (\d{2}):([0-5]{1})([0-9]{1}):([0-5]{1})([0-9]{1})$/",[$dateNews]);
+        if(empty($rgx)){
+            print("Le format de la date n'est pas valide");
+        }else {
+            $this->dateNews = $dateNews;
+        }
+    }
 
 
     // Getters et Setters utiles pour theUserLogin
